b88e5aae36d35f9fdf5bbad8a7d969156482c547
Virendra Dubey <virendra.dubey@inin.com>
2016-03-23 17:44:13 +0530

    added class diagram in resources

---
+++ TemeratureFinder/src/com/inin/Main.java
@@ -0,0 +1,13 @@
+package com.inin;
+
+/**
+ * Created by virendradubey on 23/3/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Temperature Finder");
+
+
+    }
+}
