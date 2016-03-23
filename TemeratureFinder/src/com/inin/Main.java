f1f81526a9f0de4724d82eacfd1572e239b3c3fa
Virendra Dubey <virendra.dubey@inin.com>
2016-03-23 18:33:13 +0530

    added code for program2 (WIP)

--- TemeratureFinder/src/com/inin/Main.java
+++ TemeratureFinder/src/com/inin/Main.java
@@ -1,19 +1,61 @@
 package com.inin;
 
-import java.util.HashMap;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by virendradubey on 23/3/16.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    static Map<Integer, String> entries;
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Temperature Finder");
-        Map<Integer, Map<Integer, Map<Integer, Map<Integer, Integer>>>> tempFinder = new HashMap<>();
+
+        //reading temperature file
+        try(BufferedReader fIn = new BufferedReader(new FileReader("resources/temperatureRecord.txt"))){
+
+            String line;
+            entries = new TreeMap<>();
+            int i = 0;
+
+            System.out.println(entries);
+
+            while ( (line = fIn.readLine())!=null){
+                entries.put(i++, line);
+            }
+
+            System.out.println(entries);
+        }
+
+        processEntries();
 
 
+        //output the maximum temperature in the file
+        try(BufferedWriter fOut = new BufferedWriter(new FileWriter("resources/output.txt"))){
+            fOut.write("output");
+            fOut.flush();
+        }
 
 
+    }
+
+    private static void processEntries() {
+
+        entries.forEach((k,v)->{
+            String[] values = v.split("|");
+            
+        });
     }
 }
