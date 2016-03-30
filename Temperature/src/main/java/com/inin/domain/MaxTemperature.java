4a8c9e940f65822c5d2b73e58e863e8c278d877a
mangeshsutar <mangesh.sutar@inin.com>
2016-03-30 18:52:55 +0530

    Implementing second project finding maximum temperature

--- Temperature/src/main/java/com/inin/domain/MaxTemperature.java
+++ Temperature/src/main/java/com/inin/domain/MaxTemperature.java
@@ -5,27 +5,25 @@ import java.util.*;
 
 /**
  * Created by root on 24/3/16.
- *
+ * Finding maximum temperature yearly basis.
  */
 public class MaxTemperature {
     public static void main(String[] args) throws IOException {
 
         File file = new File("src/main/resources/temperature/");
         File[] files = file.listFiles();
-
         Map<Integer, Integer> yearTempMap = new HashMap<Integer, Integer>();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/output.txt"));
 
         for (int i = 0; i < files.length; i++) {
 
             if (files[i].isFile()) {
-                System.out.println("inside if ");
                 String tempLine;
                 int maxTemperature = 0;
                 int maxTemperatureStation = 0;
 
-               // System.out.println(files[i].getPath());// getName());
-
                 BufferedReader bufferedReader = new BufferedReader(new FileReader(files[i].getPath()));
+
                 int year = 0;
                 int temperature = 0;
                 int station = 0;
@@ -38,38 +36,35 @@ public class MaxTemperature {
                         year = Integer.parseInt(str[1]);
                         temperature = Integer.parseInt(str[4]);
 
-                       // System.out.println(station + " || " + year + " || " + temp);
-
                         if (maxTemperature < temperature) {
                             maxTemperature = temperature;
                             maxTemperatureStation = station;
-                            System.out.println(maxTemperature + " || " + maxTemperatureStation);
-
-                            if (yearTempMap.containsKey(year)) {
-                                if (temperature > yearTempMap.get(year)  ) {
-                                    yearTempMap.put(year, temperature);
-                                }
-                            } else {
+                        }
+                        if (yearTempMap.containsKey(year)) {
+                            if (temperature > yearTempMap.get(year)  ) {
                                 yearTempMap.put(year, temperature);
                             }
+                        } else {
+                            yearTempMap.put(year, temperature);
                         }
-                        //System.out.println(maxTemp + " || " + maxTempStation);
                     }
                 }
 
-                System.out.println(yearTempMap);
+                bufferedWriter.write("===================== File Name " + files[i].getPath() + " =====================\n");
+                String temperatureStationMax = "Maximum Temperature : " + maxTemperature + "  And station number : " + maxTemperatureStation + "\n";
+                bufferedWriter.write(temperatureStationMax);
+                bufferedWriter.write("Year wise Maximum temperature :: \n");
+                Set<Map.Entry<Integer, Integer>> data = yearTempMap.entrySet();
+                for (Map.Entry entry : data) {
+                    String str = "Year : " + entry.getKey() + " Maximum temperature : " + entry.getValue() + "\n";
+                    bufferedWriter.write(str);
+                }
+                bufferedWriter.flush();
+                bufferedReader.close();
+
             }
         }
-
-
-        /*for (File file1 : files){
-            BufferedReader reader = new BufferedReader(new FileReader(file1));
-            String line =  reader.readLine();
-            while (line != null){
-
-            }
-        }
-*/
-
+        bufferedWriter.write("================================================================================\n");
+        bufferedWriter.close();
     }
 }
