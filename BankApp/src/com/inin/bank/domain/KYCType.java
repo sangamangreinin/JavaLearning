7237f4d526e945b8ef377afd31db3979b953158e
Lokesh Lodha <lokesh.lodha@inin.com>
2016-04-05 17:14:26 +0530

    removed other projects from this tree

--- BankApp/src/com/inin/bank/domain/KYCType.java
+++
@@ -1,9 +0,0 @@
-package com.inin.bank.domain;
-
-/**
- * Created by lokesh on 23/3/16.
- * Enum Representation of KYC Document Type
- */
-public enum KYCType {
-    PAN, PASSPORT, AADHAR, DRIVINGLICENSE
-}
--- BankApp/src/com/inin/bank/domain/TransactionType.java
+++
@@ -1,9 +0,0 @@
-package com.inin.bank.domain;
-
-/**
- * Created by lokesh on 23/3/16.
- * Enum representing Transaction Types
- */
-public enum TransactionType {
-    CASHDEPOSIT,CHEQUEDEPOSIT, CASHWITHDRAWAL, CHEQUETRANSFER
-}
