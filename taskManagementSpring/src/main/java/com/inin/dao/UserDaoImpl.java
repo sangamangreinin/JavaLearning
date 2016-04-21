5da902b7c82fa444237cf657d9139b0a916283fc
swetasharma <sweta.sharma@inin.com>
2016-04-21 12:08:19 +0530

    removed unused imports

--- taskManagementSpring/src/main/java/com/inin/dao/UserDaoImpl.java
+++ taskManagementSpring/src/main/java/com/inin/dao/UserDaoImpl.java
@@ -10,9 +10,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-import java.time.LocalDateTime;
 
 /**
  * This class consist of all the DB related operations for user
